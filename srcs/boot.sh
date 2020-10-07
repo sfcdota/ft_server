96291b4f69f5a324dcb1e8a32407c41984e96655
Christine Bach <cbach@er-d6.kzn.21-school.ru>
2020-10-07 16:46:32 +0300

    haha

--- srcs/boot.sh
+++ srcs/boot.sh
@@ -1,21 +1,23 @@
 #phpMyAdmin and wordpress from srcs (configured)
-mv -t /var/www/html/ phpMyAdmin
+mv -t /var/www/html/ phpmyadmin
 mv wordpress/* /var/www/html/
-chown -R www-data /var/www/html/*
-chmod -R 755 /var/www/html/*
+
 
 #NGINX
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
 mv localhost /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
+chown -R www-data /var/www/html/*
+chmod -R 755 /var/www/html/*
 #cp localhost.conf /etc/nginx/conf.d/
 
 service mysql start
 
 mysql -u root --skip-password -e "\
 		CREATE DATABASE wordpress;\
-		GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';\
+		GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';
+		UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='root';\
 		FLUSH PRIVILEGES;\
 		EXIT" &
 
