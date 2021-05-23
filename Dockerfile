08295d5f4e7b3b4ea8c32ae3c5e484b0717dfebe
sfcdota <sfcdota@gmail.com>
2021-05-24 00:33:12 +0300

    cleaner sh

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,21 @@
 FROM debian:buster
-
-LABEL maintainer = "cbach"
-
 WORKDIR /dir/
-
-RUN apt-get update -y
-
-RUN apt-get upgrade -y
-
-RUN apt-get install nginx -y
-
-RUN apt-get install -y \
-	php php-mysql php-fpm \
-	php-curl php-gd php-intl php-mbstring php-soap \
-	php-xml php-xmlrpc php-zip default-mysql-server
-
 COPY ./srcs/ .
+RUN apt-get update -y && apt-get update -y && apt-get install -y \
+	nginx php php-mysql php-fpm \
+	php-curl php-gd php-intl php-mbstring php-soap \
+	php-xml php-xmlrpc php-zip default-mysql-server && \
+	mv -t /var/www/html/ phpMyAdmin && \
+	mv wordpress/* /var/www/html/ && \
+	rm /etc/nginx/sites-available/default && \
+	rm /etc/nginx/sites-enabled/default && \
+	mv localhost /etc/nginx/sites-available/ && \
+	ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost && \
+	chown -R www-data /var/www/html/* && \
+	chmod -R 755 /var/www/html/* && \
+	openssl req -x509 -nodes -newkey rsa:2048 \
+	-keyout /etc/ssl/certs/localhost.key -out /etc/ssl/certs/localhost.pem \
+	-subj "/C=RU/ST=Kazan/L=Kazan/O=21School/OU=cbach/CN=localhost"
 
 EXPOSE 80 443
 
--- srcs/boot.sh
+++ srcs/boot.sh
@@ -1,30 +1,9 @@
-#phpMyAdmin and wordpress from srcs (configured)
-mv -t /var/www/html/ phpMyAdmin
-mv wordpress/* /var/www/html/
-
-
-#NGINX
-rm /etc/nginx/sites-available/default
-rm /etc/nginx/sites-enabled/default
-mv localhost /etc/nginx/sites-available/
-ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
-chown -R www-data /var/www/html/*
-chmod -R 755 /var/www/html/*
-openssl req -x509 -nodes -newkey rsa:2048 \
--keyout /etc/ssl/certs/localhost.key -out /etc/ssl/certs/localhost.pem \
--subj "/C=RU/ST=Kazan/L=Kazan/O=21School/OU=cbach/CN=localhost"
-
-
 service mysql start
 mysql -u root --skip-password -e "\
-		CREATE DATABASE wordpress;\
-		GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';
-		UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='root';\
-		FLUSH PRIVILEGES;\
-		EXIT" &
-
+			CREATE DATABASE wordpress;\
+			GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';\
+			UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='root';\
+			FLUSH PRIVILEGES;"
 service php7.3-fpm start
-#nginx -g 'daemon off;'
 service nginx start
-
-bash
+sleep infinity
